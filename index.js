48d3c1b956783b440b20d22962149fb6358aea19
sailajakankipati <147037219+sailajakankipati@users.noreply.github.com>
2025-01-22 14:15:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const convert = require('./convert');
+
+const app = express();
+app.use(express.json());
+
+app.get('/convert', (req, res) => {
+    const { inr } = req.query;
+    if (!inr) {
+        return res.status(400).json({ error: 'Missing required query parameters' });
+    }
+
+    try {
+        const usd = convert(inr);
+        res.json({ inr, usd});
+    } catch (e) {
+        res.status(500).json({ error: e });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
